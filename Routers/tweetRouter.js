dd034f6272049188a6e6a69a0d4966146c0f3fe5
janep <bp5421@srmist.edu.in>
2023-09-13 23:09:33 +0530

    Added getAllTweets API

--- Routers/tweetRouter.js
+++ Routers/tweetRouter.js
@@ -12,8 +12,8 @@ app.use(express.json())
 
 router.post("/",usercontroller.protect, tweetcontroller.createTweet);
 router.delete("/:id",usercontroller.protect, tweetcontroller.deleteTweet);
-router.put("/:id/like",usercontroller.protect,tweetcontroller.likeOrDislike);
-router.get("/timeline",usercontroller.protect,tweetcontroller.getAllTweets);
+router.put("/like/:id",usercontroller.protect,tweetcontroller.likeOrDislike);
+router.get("/getAllTweets",usercontroller.protect,tweetcontroller.getAllTweets);
 // router.get("/user/all/:id",usercontroller.protect, getUserTweets);
 // router.get("/explore", getExploreTweets);
 
